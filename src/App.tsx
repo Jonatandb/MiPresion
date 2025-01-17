ded3ae3272ec2d7efabcdc0e9cb7d9bd81615757
Jonatandb <jonatandb@gmail.com>
2025-01-17 10:23:55 -0300

    AddButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import Header from "./components/Header/Header"
 import LogsList from "./components/LogsList/LogsList"
+import AddButton from "./components/AddButton/AddButton"
 
 const App = () => {
+  const [showAddButton, setShowAddButton] = useState(true)
+
+  const handleAddButton = () => {
+    alert("Agregar nuevo registro")
+  }
+
   return (
     <>
       <Header />
       <LogsList />
+
+      {
+        showAddButton && <AddButton onClick={handleAddButton} />
+      }
     </>
   )
 }
---
+++ src/components/AddButton/AddButton.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+import add from '../../assets/add_white.png'
+
+const AddButton = ({ onClick }: { onClick: () => void }) => {
+  const handleClick = () => {
+    onClick()
+  }
+
+  return (
+    <button id="addButton" onClick={handleClick}>
+      <img src={add} height={30} alt="Ícono agregar nuevo registro" title="Agregar registro"></img>
+    </button>
+  )
+}
+
+export default AddButton
