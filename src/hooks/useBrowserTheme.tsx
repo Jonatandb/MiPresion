1dd5b189ead8c1c2f4c0f76006b0585ffed2c627
Jonatandb <jonatandb@gmail.com>
2025-01-17 17:57:34 -0300

    Add useBrowserTheme hook

---
+++ src/hooks/useBrowserTheme.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export const useBrowserTheme = () => {
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem('theme');
+    return storedTheme || 'light';
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return { theme, toggleTheme };
+};
