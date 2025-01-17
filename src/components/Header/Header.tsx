8c7916b2d6c210b299fb98fe373590dfc069b8a6
Jonatandb <jonatandb@gmail.com>
2025-01-17 00:53:26 -0300

    Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h2>
+        Logs
+      </h2>
+      <img id="logo" src="/favicon.ico" alt="favicon" height={30} />
+    </header>
+  )
+}
+
+export default Header
