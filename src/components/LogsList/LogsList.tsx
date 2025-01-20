91fe4d2b267c7665106c426e7679ae05a0bdb5d3
Jonatandb <jonatandb@gmail.com>
2025-01-20 20:12:38 -0300

    Refactor to use CSS modules in LogsList component

--- src/components/LogsList/LogsList.tsx
+++ src/components/LogsList/LogsList.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import Log from '@/components/Log/Log'
 import { LogContext } from '@/contexts/LogContext'
 
-import './styles.css'
+import styles from './LogsList.module.css'
 
 const LogsList = () => {
   const { logs } = useContext(LogContext)
@@ -20,7 +20,7 @@ const LogsList = () => {
   };
 
   return (
-    <section id="logsListContainer">
+    <section className={styles.logsListContainer}>
       {
         logs.map(log => (
           <Log key={log.id} {...log} date={formatDate(log.date)} />
