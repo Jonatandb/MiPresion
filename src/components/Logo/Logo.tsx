ee5b1cf77cb8b36db3fda87f47d27cd59f926623
Jonatandb <jonatandb@gmail.com>
2025-01-21 20:07:44 -0300

    Optimizations suggested by Lighthouse

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,10 @@
 
-import faviconWhite from '@/assets/favicon_white.png'
+import favicon60 from '@/assets/favicon_60x60.png'
+import favicon120 from '@/assets/favicon_120x120.png'
 
 import styles from './Logo.module.css'
 
-const Logo = () => <img className={`${styles.logo}`} src={faviconWhite} alt="Logo principal" title='Registro de Presión Sanguínea por Jonatandb' height={30} width={30} />
+const Logo = () => <img className={`${styles.logo}`} src={favicon60} srcSet={`${favicon60} 60w, ${favicon120} 120w`} alt="Logo de MiPresión - Registro de Presión Sanguínea por Jonatandb" title='MiPresión - Registro de Presión Sanguínea por Jonatandb' height={30} width={30} />
 
 
 export default Logo
