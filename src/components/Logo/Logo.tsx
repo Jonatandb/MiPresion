b4e626ddf92fe2fe234e8dc7ef6348db70ab0abb
Jonatandb <jonatandb@gmail.com>
2025-01-23 01:10:48 -0300

    Update Eslint rules

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,8 @@
 
-import favicon60 from '@/assets/favicon_60x60.png'
-import favicon120 from '@/assets/favicon_120x120.png'
+import favicon60 from "@/assets/favicon_60x60.png"
+import favicon120 from "@/assets/favicon_120x120.png"
 
-import styles from './Logo.module.css'
+import styles from "./Logo.module.css"
 
 const images = [
   favicon60,
@@ -19,7 +19,7 @@ const Logo = () => {
       <img className={`${styles.logo}`} src={favicon60} srcSet={`${favicon60} 60w, ${favicon120} 120w`} alt="Logo de MiPresión - Registro de Presión Sanguínea por Jonatandb" title='MiPresión - Registro de Presión Sanguínea por Jonatandb' height={30} width={30} />
       <div className={`${styles.appTitle}`}>
         <h1>
-          MiPresión <span>v.1.2.1</span>
+          MiPresión <span>v.1.2.2</span>
         </h1>
         <span>
           Registro de presión sanguínea
