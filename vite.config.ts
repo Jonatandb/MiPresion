7ff36e5fa5d8a329ccc425a592f70adf4850a35c
Jonatandb <jonatandb@gmail.com>
2025-01-25 13:27:59 -0300

    Add better website favicons and open graph meta tags and image

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { fileURLToPath, URL } from "url"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     {
-      name: 'rewrite-paths',
-      enforce: 'post',
+      name: "rewrite-paths",
+      enforce: "post",
       generateBundle(options, bundle) {
         const basePath = mode === "development" ? "/" : "/MiPresion/";
         Object.values(bundle).forEach((chunk) => {
-          if (chunk.type === 'chunk' || chunk.type === 'asset') {
-            if ('code' in chunk) {
+          if (chunk.type === "chunk" || chunk.type === "asset") {
+            if ("code" in chunk) {
               chunk.code = chunk.code
                 .replace(/["']\/assets\//g, `"${basePath}assets/`)
             }
@@ -28,9 +28,9 @@ export default defineConfig(({ mode }) => ({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@/contexts': fileURLToPath(new URL('./src/contexts', import.meta.url)),
-      '@/hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@/contexts": fileURLToPath(new URL("./src/contexts", import.meta.url)),
+      "@/hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
     }
   },
 }))
