57e720fb2d2b461635e6c30c199cbebb065c4dc9
Jonatandb <jonatandb@gmail.com>
2025-02-05 19:00:55 -0300

    Update production build -> v3.4.6

---
+++ docs/components/SEO/SEO.d.ts
@@ -0,0 +1,6 @@
+declare const Seo: ({ title, description, keywords }: {
+    title?: string | undefined;
+    description?: string | undefined;
+    keywords?: string | undefined;
+}) => import("react/jsx-runtime").JSX.Element;
+export default Seo;
